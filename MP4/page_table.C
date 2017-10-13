79c1f24de31de7ece62b71df7a87803fefa48fc6
Duy Nguyen <nguyenhaduy@tamu.edu>
2017-10-13 02:46:15 -0500

    Put the page directory table into process mem pool

--- MP4/page_table.C
+++ MP4/page_table.C
@@ -29,12 +29,13 @@ PageTable::PageTable()
     //Only initialize if there is no page directory
     if (page_directory == NULL){
       //Put the page_directory in kernel_mem_pool
-      page_directory = ( unsigned long*)(kernel_mem_pool->get_frames(1)*PAGE_SIZE); 
+      // page_directory = ( unsigned long*)(kernel_mem_pool->get_frames(1)*PAGE_SIZE); 
+      page_directory = ( unsigned long*)(process_mem_pool->get_frames(1)*PAGE_SIZE); 
       
 
       //Direct mapped page_table
       //Get frame for kernel page table (first 4MB)
-      unsigned long *page_table = (unsigned long*)(kernel_mem_pool->get_frames(1)*PAGE_SIZE); 
+      unsigned long *page_table = (unsigned long*)(process_mem_pool->get_frames(1)*PAGE_SIZE); 
       unsigned long  memory_addr = 0;
 
       for (unsigned int i = 0; i < ENTRIES_PER_PAGE; i++){
@@ -43,8 +44,8 @@ PageTable::PageTable()
         memory_addr += PAGE_SIZE;
       }
 
-      //Set the first entry of page directory to 'kernel', 'r&w', 'present' -> 011
-      page_directory[0] = (unsigned long) page_table | 0x3;
+      //Set the first entry of page directory to 'user', 'r&w', 'present' -> 011
+      page_directory[0] = (unsigned long) page_table | 0x7;
     }
 
     // Set all page table entries in the directory to 'kernel', 'r&w', ' not present' -> 010
@@ -96,7 +97,11 @@ void PageTable::handle_fault(REGS * _r)
     else { // If age not present
 
       //Read page directory from register CR3
-      unsigned long *page_dir = (unsigned long *) read_cr3();      
+      // unsigned long *page_dir = (unsigned long *) read_cr3();      
+
+      // If the page directory table is from the process_mem_pool
+      unsigned long *page_dir = (unsigned long *) 0xFFFFF000; // logical address to access page directory table
+
 
       //Read the fault page address from register CR2
       unsigned long memory_addr = read_cr2();
@@ -108,38 +113,37 @@ void PageTable::handle_fault(REGS * _r)
       unsigned long page_dir_index = memory_addr / (ENTRIES_PER_PAGE*PAGE_SIZE);
 
       VMPool** VMPool_Array = current_page_table->VM_Pools;
-      unsigned int VM_Index = -1;
-
+      bool legitimated = false;
       for (unsigned int i = 0; i < 16; ++i) {
       	if ((VMPool_Array[i]!=NULL) && (VMPool_Array[i]->is_legitimate(memory_addr))) {
-      		VM_Index = i;
+      		legitimated = true;
       		break;
       	}
       }
 
       // If the address is not legitimate in any pools
-      if (VM_Index == -1) {
-      	Console::puts("Address is invalid!!!\n");
+      if (legitimated) {
+      	Console::puts("Address is not legitimated!!\n");
       	// return;
       }
 
 	    //If the 2nd level page_table is in memory
 	  if(page_dir[page_dir_index] & 0x1 == 0x1) {  
-	    //Get page table from page directory
-	    page_table = (unsigned long *) ((page_dir[page_dir_index]) & 0xFFFFF000);
-	    // page_table = (unsigned long *) ((page_dir_index*PAGE_SIZE) | 0xFFC00000);
+	    // Get page table from page directory
+	    // page_table = (unsigned long *) ((page_dir[page_dir_index]) & 0xFFFFF000);
+	    page_table = (unsigned long *) ((page_dir_index*PAGE_SIZE) | 0xFFC00000);
 	  } 
 	  //If the 2nd level page_table is not loaded in memory
 	  else {  
-	    //Get address of new page table      
-	    page_table = (unsigned long*)(kernel_mem_pool->get_frames(1)*PAGE_SIZE);
-	    //Load page_table into memory
-	    page_dir[page_dir_index] = (unsigned long)(page_table) | 0x3;
-	    // page_dir[page_dir_index] = (unsigneir_index*PAGE_SIZE) | 0xFFC00000);
+	    // Get address for new page table      
+	    page_dir[page_dir_index] = (unsigned long)(process_mem_pool->get_frames(1)*PAGE_SIZE);
+	    // Get the page table logical address
+	    // page_table = (unsigned long *) ((page_dir[page_dir_index]) & 0xFFFFF000);
+	    page_table = (unsigned long *) ((page_dir_index*PAGE_SIZE) | 0xFFC00000);
 
 	    //Initialize all entry of the page table to empty
 	    for(unsigned int i = 0; i < ENTRIES_PER_PAGE; i++){
-	        page_table[i] = 0x0;
+	        page_table[i] = 0x2;
 	    }
 	  }
 
@@ -157,8 +161,8 @@ void PageTable::free_page (unsigned long _page_no) {
 	// Get frame number from a logical address
 	unsigned long page_dir_index = _page_no / (ENTRIES_PER_PAGE*PAGE_SIZE);
 	unsigned long page_tab_index = (_page_no / PAGE_SIZE) & 0x3FF;
-	// unsigned long* page_table = (unsigned long*)(0xFFC00000 | (page_dir_index * PAGE_SIZE));
-	unsigned long* page_table = (unsigned long*) (page_directory[page_dir_index] * PAGE_SIZE);
+	// unsigned long* page_table = (unsigned long*) (page_directory[page_dir_index] * PAGE_SIZE);
+	unsigned long* page_table = (unsigned long*)((page_dir_index * PAGE_SIZE) | 0xFFC00000);
 	unsigned long frame_number = page_table[page_tab_index];
 
 	// Release the frame
@@ -166,6 +170,7 @@ void PageTable::free_page (unsigned long _page_no) {
 
 	// Mark the page invalid
 	page_table[page_tab_index] = 0x0;
+	write_cr3(read_cr3());
 }
 
 void PageTable::register_pool (VMPool *_pool) {
