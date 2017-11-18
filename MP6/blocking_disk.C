ad40a53db21810dc3ff29370d4ce83b5b68c2b46
Duy Nguyen <nguyenhaduy@tamu.edu>
2017-11-18 12:32:50 -0600

    Add a thread queue to blocking_disk.C

--- MP6/blocking_disk.C
+++ MP6/blocking_disk.C
@@ -22,13 +22,17 @@
 #include "utils.H"
 #include "console.H"
 #include "blocking_disk.H"
+#include "machine.H"
 
 /*--------------------------------------------------------------------------*/
 /* CONSTRUCTOR */
 /*--------------------------------------------------------------------------*/
+// bool SimpleDisk::write_lock = false;
+
 
 BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size) 
   : SimpleDisk(_disk_id, _size) {
+  	write_lock = false;
 }
 
 /*--------------------------------------------------------------------------*/
@@ -36,13 +40,84 @@ BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size)
 /*--------------------------------------------------------------------------*/
 
 void BlockingDisk::read(unsigned long _block_no, unsigned char * _buf) {
-  // -- REPLACE THIS!!!
-  SimpleDisk::read(_block_no, _buf);
+  	// register the thread that is to block as well as the corresponding disk number
+	block_queue.enqueue(Thread::CurrentThread());
+	
+	// tell the disk the thread is about to apply I/O operation
+	issue_operation(READ, _block_no);
+	
+	// any time the thread gains CPU, it checks if the disk is ready
+	while (!is_ready()) {
+		// if not, gives up CPU and wait
+		SYSTEM_SCHEDULER->resume(Thread::CurrentThread());
+		SYSTEM_SCHEDULER->yield();
+	}
+	
+	// if yes, apply I/O operation;
+	int i;
+	unsigned short tmpw;
+	for (i = 0; i < 256; i++) {
+		tmpw = Machine::inportw(0x1F0);
+		_buf[i*2]   = (unsigned char)tmpw;
+		_buf[i*2+1] = (unsigned char)(tmpw >> 8);
+	}
+
+	// after the I/O operation is done
+	// dequeue the thread in the block queue
+	// dequeue the disk in the disk queue
+	head = block_queue.dequeue();
 
 }
 
 
 void BlockingDisk::write(unsigned long _block_no, unsigned char * _buf) {
-  // -- REPLACE THIS!!!
-  SimpleDisk::read(_block_no, _buf);
+  while (write_lock) {}   //wait other threads to finish writing
+	write_lock = true;   //set the lock
+	
+	// register the thread that is to block as well as the corresponding disk
+	block_queue.enqueue(Thread::CurrentThread());
+
+
+	// tell the disk the thread is about to apply I/O operation
+	issue_operation(WRITE, _block_no);
+	
+	// any time the thread gains CPU, it checks if the disk is ready
+	// If the disk is not ready, yield the CPU to other threads
+	while (!is_ready()) {
+		SYSTEM_SCHEDULER->resume(Thread::CurrentThread());
+		SYSTEM_SCHEDULER->yield();
+	}
+	
+	// If the disk is ready, write data to port
+	int i; 
+	unsigned short tmpw;
+	for (i = 0; i < 256; i++) {
+		tmpw = _buf[2*i] | (_buf[2*i+1] << 8);
+		Machine::outportw(0x1F0, tmpw);
+	}
+	// after the I/O operation is done
+	// dequeue the thread in the block queue
+	// dequeue the disk in the disk queue
+	head = block_queue.dequeue();
+
+	write_lock = false;   //free the lock
+}
+
+
+void BlockingDisk::interrupt_handler(REGS* _regs) {
+	if (SYSTEM_SCHEDULER->lock) {
+		return;
+	}
+	// whenever the disk is ready to have I/O operation, this interrupt is trigerred
+	Thread* thread_to_active = block_queue[head];
+	Thread* thread_to_active = block_queue.front();
+	
+	if (thread_to_active == Thread::CurrentThread()) {
+		// if this is the case, simply return and do no operations
+		return;
+	}
+	else {
+		SYSTEM_SCHEDULER->preempt(thread_to_active);
+		return;
+	}
 }
